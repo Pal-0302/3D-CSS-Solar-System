133edb971b1b8f4dc359eb3889df6f4fe0649d76
Pal-0302 <pallu200203@gmail.com>
2024-09-08 16:03:44 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -23,11 +23,44 @@ $(window).load(function(){
     e.preventDefault();
   });
 
+  // Function to send data to ESP8266
+  var sendToESP8266 = function(planetNumber) {
+    fetch(`http://your-esp8266-ip/send?number=${planetNumber}`, {
+      method: 'GET'
+    })
+    .then(response => response.text())
+    .then(data => {
+      console.log("Sent to ESP8266: " + planetNumber);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  };
+
   $("#data a").click(function(e) {
     var ref = $(this).attr("class");
     solarsys.removeClass().addClass(ref);
     $(this).parent().find('a').removeClass('active');
     $(this).addClass('active');
+    
+    // Assign a unique number to each planet
+    var planetNumber;
+    switch(ref) {
+      case 'mercury': planetNumber = 1; break;
+      case 'venus': planetNumber = 2; break;
+      case 'earth': planetNumber = 3; break;
+      case 'mars': planetNumber = 4; break;
+      case 'jupiter': planetNumber = 5; break;
+      case 'saturn': planetNumber = 6; break;
+      case 'uranus': planetNumber = 7; break;
+      case 'neptune': planetNumber = 8; break;
+      case 'pluto': planetNumber = 9; break;
+      default: planetNumber = 0; break;
+    }
+
+    // Send the planet number to the ESP8266
+    sendToESP8266(planetNumber);
+
     e.preventDefault();
   });
 
